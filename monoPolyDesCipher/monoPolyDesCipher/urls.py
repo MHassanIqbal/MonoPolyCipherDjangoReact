41ea8a1d0fac2c717d8faa61adbed27e86db0b55
MHassanIqbal <mhassaniqbal22@gmail.com>
2024-05-12 18:26:18 +0200

    React successfully integrated within Python

--- monoPolyDesCipher/monoPolyDesCipher/urls.py
+++ monoPolyDesCipher/monoPolyDesCipher/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from django.shortcuts import render
+
+def index_view(request):
+    return render(request, 'dist/index.html')
 
 urlpatterns = [
-    path('', include('cipher.urls')),
+    # path('', include('cipher.urls')),
+    path('', index_view, name='index'),
 ]
